f3843cf36bd020f1679df4ab54b854f938db55e1
trippsc2 <46848743+trippsc2@users.noreply.github.com>
2025-01-10 13:57:08 -0500

    Revisted testing to include Fedora 41 and also to move Rocky 9 to the primary platform.

--- molecule-tests.sh
+++ molecule-tests.sh
@@ -2,13 +2,16 @@
 
 set -e
 
-MOLECULE_BOX="rocky8_cis" molecule test -s combine_certificates
+MOLECULE_BOX="rocky9_cis" molecule test -s combine_certificates
 MOLECULE_BOX="debian12_base" molecule test -s combine_certificates
+MOLECULE_BOX="fedora41_base" molecule test -s combine_certificates
 MOLECULE_BOX="ubuntu2404_base" molecule test -s combine_certificates
 
-MOLECULE_BOX="rocky9_base" molecule test -s combine_certificates
+MOLECULE_BOX="rocky8_cis" molecule test -s combine_certificates
 
 MOLECULE_BOX="debian11_base" molecule test -s combine_certificates
 
 MOLECULE_BOX="ubuntu2204_base" molecule test -s combine_certificates
 MOLECULE_BOX="ubuntu2004_base" molecule test -s combine_certificates
+
+molecule test -s tz
