cd058c4c3f0ab27274f381d71c185c768c97d482
Jim Tarpley <46848743+trippsc2@users.noreply.github.com>
2025-03-05 08:43:05 -0500

    More test cleanup.

--- molecule-tests.sh
+++ molecule-tests.sh
@@ -3,8 +3,8 @@
 set -e
 
 MOLECULE_BOX="rocky9_cis" molecule test -s combine_certificates
-MOLECULE_BOX="debian12_base" molecule test -s combine_certificates
 MOLECULE_BOX="fedora41_base" molecule test -s combine_certificates
+MOLECULE_BOX="debian12_base" molecule test -s combine_certificates
 MOLECULE_BOX="ubuntu2404_base" molecule test -s combine_certificates
 
 MOLECULE_BOX="rocky8_cis" molecule test -s combine_certificates
